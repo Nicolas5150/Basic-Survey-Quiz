468469d6cebe696f0cbe0b5365ce4d27ca7aae25
Nicolas5150 <Nicolas@Nicolas-MacBook-Pro.local>
2016-11-16 20:22:41 -0500

    Update on form
    Adding parameter to see if an error was created during the check of the
    data being validated.

--- js/process_form.js
+++ js/process_form.js
@@ -1,76 +1,71 @@
 window.addEventListener('load',init,false);
 function init() {
   // The event listener for click as well as when the corresponding text box is
-  // clicked on or not clicked on
+  // clicked on (focus) or not clicked on (blur)
   var firstNameTextBox = document.getElementById("firstNameTextBox");
   firstNameTextBox.addEventListener("focus",function(){
-    formAssistAdd(event, "firstNameMessage");}, true);
+    formAssistAdd(event, "firstNameMessage", "");}, true);
   firstNameTextBox.addEventListener("blur",function(){
-    formAssistRemove(event, "firstNameMessage");}, true);
+    formAssistRemove(event, "firstNameMessage", "");}, true);
 
   var lastNameTextBox = document.getElementById("lastNameTextBox");
   lastNameTextBox.addEventListener("focus",function(){
-    formAssistAdd(event, "lastNameMessage");}, true);
+    formAssistAdd(event, "lastNameMessage", "");}, true);
   lastNameTextBox.addEventListener("blur",function(){
-    formAssistRemove(event, "lastNameMessage");}, true);
+    formAssistRemove(event, "lastNameMessage", "");}, true);
 
   var emailAddressTextBox = document.getElementById("emailAddressTextBox");
   emailAddressTextBox.addEventListener("focus",function(){
-    formAssistAdd(event, "emailAddressMessage");}, true);
+    formAssistAdd(event, "emailAddressMessage", "");}, true);
   emailAddressTextBox.addEventListener("blur",function(){
-    formAssistRemove(event, "emailAddressMessage");}, true);
+    formAssistRemove(event, "emailAddressMessage", "");}, true);
 
   var phoneNumberTextBox = document.getElementById("phoneNumberTextBox");
   phoneNumberTextBox.addEventListener("focus",function(){
-    formAssistAdd(event, "phoneNumberMessage");}, true);
+    formAssistAdd(event, "phoneNumberMessage", "");}, true);
   phoneNumberTextBox.addEventListener("blur",function(){
-    formAssistRemove(event, "phoneNumberMessage");}, true);
+    formAssistRemove(event, "phoneNumberMessage", "");}, true);
 
   var sulleyAddressTextBox = document.getElementById("sulleyAddressTextBox");
   sulleyAddressTextBox.addEventListener("focus",function(){
-    formAssistAdd(event, "sulleyAddressMessage");}, true);
+    formAssistAdd(event, "sulleyAddressMessage", "");}, true);
   sulleyAddressTextBox.addEventListener("blur",function(){
-    formAssistRemove(event, "sulleyAddressMessage");}, true);
-
-}
-
-// Once the user has clicked the submit button then validate the data
-function validateData(){
-  event.preventDefault();
-  var firstNameTextBox = document.getElementById('firstNameTextBox');
-  var lastNameTextBox = document.getElementById('lastNameTextBox');
-  var emailAddressTextBox = document.getElementById('emailAddressTextBox');
-  var phoneNumberTextBox = document.getElementById('phoneNumberTextBox');
-  var sulleyAddressTextBox = document.getElementById('sulleyAddressTextBox');
+    formAssistRemove(event, "sulleyAddressMessage", "");}, true);
 }
 
 // Add data / text to the div argument passed in
-function formAssistAdd(event, clickID){
+function formAssistAdd(event, clickID, error){
   event.preventDefault();
   // Remove all tags on current div
-  formAssistRemove(event, clickID);
-
-  // Switch Statment to add correct appeded text to screen
+  formAssistRemove(event, clickID, error);
   var info = "";
+  // Check to see if an error was passed in
+  if (error != ""){
+    info = "Incorrect ";
+  }
+  // Switch Statment to append correct example to screen
   switch(clickID) {
-    case "firstNameMessage":
-      info = "Ex. John";
+    case "firstNameMessageErr":
+      info += "Incorrect ";
+
+    case ("firstNameMessage"):
+      info += "Ex. John";
       break;
 
     case "lastNameMessage":
-      info = "Ex. Doe";
+      info += "Ex. Doe";
       break;
 
     case "emailAddressMessage":
-      info = "Ex. johndoe@gmail.com";
+      info += "Ex. johndoe@gmail.com";
       break;
 
     case "phoneNumberMessage":
-      info = "Ex. 555-5555-555";
+      info += "Ex. 555-5555-555";
       break;
 
     case "sulleyAddressMessage":
-      info = "Ex. http://sulley.cah.ucf.edu/~jo555555";
+      info += "Ex. http://sulley.cah.ucf.edu/~jo555555";
       break;
 
     default:
@@ -85,10 +80,21 @@ function formAssistAdd(event, clickID){
 }
 
 // Remove any data on the div argument passed in
-function formAssistRemove(event, clickID){
+function formAssistRemove(event, clickID, errorTextBox){
   event.preventDefault();
   var removeByID = document.getElementById(clickID);
   while(removeByID.hasChildNodes()) {
     removeByID.removeChild(removeByID.lastChild);
   }
+}
+
+// Once the user has clicked the submit button then validate the data
+function validateData(){
+  event.preventDefault();
+  var firstNameTextBox = document.getElementById('firstNameTextBox');
+  var lastNameTextBox = document.getElementById('lastNameTextBox');
+  var emailAddressTextBox = document.getElementById('emailAddressTextBox');
+  var phoneNumberTextBox = document.getElementById('phoneNumberTextBox');
+  var sulleyAddressTextBox = document.getElementById('sulleyAddressTextBox');
+  formAssistAdd(event,"firstNameMessage", "error");
 }
